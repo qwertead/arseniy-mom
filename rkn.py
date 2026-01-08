d210f331080c332dca93381d4ea454718eb795e6
qwertead <qwertead5@gmail.com>
2026-01-09 02:31:45 +0300

    Update rkn.py
    Исправь хуйню со вставкой

--- rkn.py
+++ rkn.py
@@ -16,17 +16,44 @@ root.title("test")
 root.geometry("800x800")
 root.configure(bg="grey")
 
+def neww():             #создание нового окна для пополнения
+    rootp = Tk()               
+    rootp.geometry("400x400")
+    rootp.configure(bg='grey')
+    btnp = ttk.Button(rootp ,text="Пополнить", width=30)
+    btnp.place(x=100, y=350)
+    lab1 = ttk.Label(rootp ,text="Введите сумму", background="grey", font=('Arial', 20))
+    lab1.place(x=110, y=50)
+    
+    
+
+def newww():               #окно для вычитания
+    global x
+    rootm = Tk()
+    rootm.geometry("400x400")
+    rootm.configure(bg="grey")
+    btnm = ttk.Button(rootm, text="Вычесть", width=30, command=cl2)
+    btnm.place(x=100, y=350)
+    lab2 = ttk.Label(rootm ,text="Введите сумму", background="grey", font=('Arial', 20))
+    lab2.place(x=110, y=50)
+    entry2 = ttk.Entry(rootm)
+    entry2.place(x=120, y=175, width=175, height=50)
+    def cl2():
+        global x
+        plus2 = entry2.get()
+        label['text'] = x - int(plus2)
+    
 
 #Кнопки
 style = ttk.Style(root)
 style.configure("TButton", font=('Arial', 14), height=100, )
-btn1 = ttk.Button(text="Пополнить", width=15, style="TButton", )
+btn1 = ttk.Button(text="Пополнить", width=15, style="TButton", command=neww )
 btn1.place(x=400, y=200)
-btn2 = ttk.Button(text="Вычесть", width=15, style="TButton")
+btn2 = ttk.Button(text="Вычесть", width=15, style="TButton", command=newww)
 btn2.place(x=200, y=200)
 
 
-#Текст
+#Баланс
 style.configure("Big.TLabel" ,font=("Courier", 44), )
 label = ttk.Label(text=x, style="Big.TLabel", background="grey" )
 label.place(x=375, y=100)
